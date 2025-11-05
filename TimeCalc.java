0c1ec4488d9163fd379616f79ecaf711a32ae06b
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-05 20:09:05 +0200

    completed task 1

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,25 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+		if (hours == 0){
+			hours = 24;
+		}
+		int TotalMinutes = hours*60 + minutes + minutesToAdd;
+        int TotalHours = TotalMinutes / 60;
+        int NewHours = TotalHours % 24;
+        int NewMinutes = TotalMinutes - (TotalHours * 60);
+
+		if (NewHours <= 9){
+			System.out.print("0" + NewHours + ":");
+		}else{
+			System.out.print( NewHours + ":");
+		}if (NewMinutes <= 9){
+			System.out.print("0" + NewMinutes);
+		}else{
+			System.out.print( NewMinutes);
+		}
+
     }
 }
