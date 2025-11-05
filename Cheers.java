8aca1d2d8636e9de76f851d72f2e1e77d18f1e2e
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-05 23:20:31 +0200

    completed task 3

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,20 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            int n = Integer.parseInt(args[1]);
+            word = word.toUpperCase();
+             String aList = "AEFHILMNORSX";
+            for(int i=0; i < word.length(); i++){
+                char letter = word.charAt(i);
+                if(aList.indexOf(letter) >= 0){
+                        System.out.println("Give me an " + letter + ": " + letter + "!");
+                }else{
+                     System.out.println("Give me a " + letter + ": " + letter + "!");   
+                }
+            }
+            System.out.println("What does that spell?");
+            for(int j=0; j < n; j++){
+                System.out.println(word + "!!!");
+            }
         }
 }
