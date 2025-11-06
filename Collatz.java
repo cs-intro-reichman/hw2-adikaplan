2c46a6abd56c31b9cec9441141df385d276fa984
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-06 22:39:04 +0200

    completed task 5

--- Collatz.java
+++ Collatz.java
@@ -2,39 +2,37 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    int N = Integer.parseInt(args[0]);
-		String input = args[1];
-		if (input.equals("v") == true){
+		String mode = args[1];
+		
 			for(int i=1; i<=N ; i++ ){
+				String row =  i + " " ;
 				int j=i;
 				int rounds =1;
-				System.out.print(i+ " ");
-				if(j==1){
+				do{
+					if( j % 2 != 0){
 					j = (j*3)+1;
-					System.out.print(j + " ");
-					rounds = rounds +1;
-				}else if (j!=1){
-				while(j != 1){
-				if( j % 2 != 0){
-					j = (j*3)+1;
-					System.out.print(j + " ");
+					row = row + j + " ";
 				}else{
 					j = j/2;
-					System.out.print(j + " ");
+					row = row + j + " ";
 				}
 				rounds = rounds +1;
-			    }
-				System.out.print("(" + rounds + ")");
-				System.out.println();
-			}
-		}
-			System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
-		}
-		
-		else if (input.equals("c") == true){
+				}while(j != 1);
+
+			if(mode.equals("c") == true && i == N){
+					System.out.println();
 			System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 
-		}
-		
-		
+			}else if (mode.equals("v") == true){ 
+				System.out.println(row + "(" + rounds + ")");
+			if(i == N){
+					System.out.println();
+			System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+
+			
+			
+			}
+		}	
 	}
+}
 }
