7a4bc734284d03890ec7f354b790028abe10afc9
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-06 13:45:38 +0200

    completed task 4

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		System.out.println("pi according to Java: " + Math.PI);
+		 double x = 1;
+		 double sum = 1;
+		 for (int i=1; i<=n; i++){
+			x = x +2;
+			if(i % 2 != 0){
+				sum = sum - (1/x);
+			}else{
+				sum = sum + (1/x);
+			}
+		 }
+		 sum = sum*4;
+		 System.out.println("pi, approximated:"  + sum);
+		 
+		 //problem with the result 
 	}
 }
