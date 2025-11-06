ffb9e2e92a6b61c507f9ab6e08aef14e8be95e83
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-06 13:58:19 +0200

    completed task 4

--- CalcPi.java
+++ CalcPi.java
@@ -4,14 +4,14 @@ public class CalcPi {
 	    int n = Integer.parseInt(args[0]);
 		System.out.println("pi according to Java: " + Math.PI);
 		 double x = 1;
-		 double sum = 1;
-		 for (int i=1; i<=n; i++){
-			x = x +2;
+		 double sum = 0;
+		 for (int i=0; i<n; i++){
 			if(i % 2 != 0){
 				sum = sum - (1/x);
 			}else{
 				sum = sum + (1/x);
 			}
+			x = x +2;
 		 }
 		 sum = sum*4;
 		 System.out.println("pi, approximated:"  + sum);
